5d116228c51cc6b0486bd2fd027a018adf0d36c0
fillmygaps <>
2019-03-10 17:32:20 +0100

    added comment

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args)
     {
+        //Added first project
         System.out.println("Enter first and second number:");
         Scanner inp= new Scanner(System.in);
         int num1,num2;
         num1 = inp.nextInt();
         num2 = inp.nextInt();
-        int ans;
         System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
         int choose;
         choose = inp.nextInt();
